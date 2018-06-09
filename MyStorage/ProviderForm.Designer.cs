8b3d3623346ea3bb9bb6c0b5fd365b48b496fd15
Артём Щеглов <stiw00@bk.ru>
2018-06-09 17:13:20 +0300

    fix commit

---
+++ MyStorage/ProviderForm.Designer.cs
@@ -0,0 +1,102 @@
+﻿namespace MyStorage
+{
+    partial class ProviderForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panelRemain = new System.Windows.Forms.Panel();
+            this.dataGridViewRemain = new System.Windows.Forms.DataGridView();
+            this.comboBoxProvider = new System.Windows.Forms.ComboBox();
+            this.panelRemain.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewRemain)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panelRemain
+            // 
+            this.panelRemain.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.panelRemain.AutoSize = true;
+            this.panelRemain.Controls.Add(this.dataGridViewRemain);
+            this.panelRemain.Location = new System.Drawing.Point(11, 32);
+            this.panelRemain.Margin = new System.Windows.Forms.Padding(2);
+            this.panelRemain.Name = "panelRemain";
+            this.panelRemain.Size = new System.Drawing.Size(430, 185);
+            this.panelRemain.TabIndex = 3;
+            this.panelRemain.Visible = false;
+            // 
+            // dataGridViewRemain
+            // 
+            this.dataGridViewRemain.AllowUserToAddRows = false;
+            this.dataGridViewRemain.AllowUserToDeleteRows = false;
+            this.dataGridViewRemain.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewRemain.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dataGridViewRemain.Location = new System.Drawing.Point(0, 0);
+            this.dataGridViewRemain.Margin = new System.Windows.Forms.Padding(2);
+            this.dataGridViewRemain.Name = "dataGridViewRemain";
+            this.dataGridViewRemain.ReadOnly = true;
+            this.dataGridViewRemain.RowTemplate.Height = 24;
+            this.dataGridViewRemain.Size = new System.Drawing.Size(430, 185);
+            this.dataGridViewRemain.TabIndex = 0;
+            // 
+            // comboBoxProvider
+            // 
+            this.comboBoxProvider.FormattingEnabled = true;
+            this.comboBoxProvider.Location = new System.Drawing.Point(11, 6);
+            this.comboBoxProvider.Name = "comboBoxProvider";
+            this.comboBoxProvider.Size = new System.Drawing.Size(121, 21);
+            this.comboBoxProvider.TabIndex = 4;
+            this.comboBoxProvider.SelectedIndexChanged += new System.EventHandler(this.comboBoxProvider_SelectedIndexChanged);
+            // 
+            // ProviderForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.ClientSize = new System.Drawing.Size(450, 263);
+            this.Controls.Add(this.comboBoxProvider);
+            this.Controls.Add(this.panelRemain);
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.MaximumSize = new System.Drawing.Size(698, 441);
+            this.MinimumSize = new System.Drawing.Size(185, 175);
+            this.Name = "ProviderForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Provider";
+            this.Load += new System.EventHandler(this.ProviderForm_Load);
+            this.panelRemain.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewRemain)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Panel panelRemain;
+        private System.Windows.Forms.DataGridView dataGridViewRemain;
+        private System.Windows.Forms.ComboBox comboBoxProvider;
+    }
+}
---
+++ MyStorage/ProviderForm.cs
@@ -0,0 +1,58 @@
+﻿using MySql.Data.MySqlClient;
+using MyStorage.Database;
+using MyStorage.DataModel;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MyStorage
+{
+    public partial class ProviderForm : Form
+    {
+        MySqlDataAdapter mySqlDataAdapter;
+        DataSet ds;
+        //List<Store> stores = new List<Store>();
+        List<Client> clients = new List<Client>();
+        Client cur_client = new Client();
+        //Store cur_shop = new Store();
+        BindingList<Client> bindingList;
+
+        public ProviderForm()
+        {
+            InitializeComponent();
+            panelRemain.Visible = true;
+            bindingList = new BindingList<Client>();
+            dataGridViewRemain.DataSource = bindingList;
+            GetProvider();
+            
+        }
+        private void ProviderForm_Load(object sender, EventArgs e)
+        {
+            Focus();
+        }
+        
+
+        public void GetProvider()
+        {
+            string sql = "select c.id,tc.type,c.name,c.middle_name,c.lastname " +
+                         "from clients c join typeclient tc on tc.id = c.id where cl_type_id = (select id from typeclient where type = 'provider');";
+            var list = DB.Select(sql);
+            clients.Clear();
+            list.ForEach(l => { var ls = l.Split(' '); clients.Add(new Client(Convert.ToInt32(ls[0]), ls[1], ls[2], ls[3], ls[4])); });
+            clients.ForEach(c => comboBoxProvider.Items.Add(c.Name + " " + c.Last_name));
+        }
+        // may repeat because not clear binding list
+        private void comboBoxProvider_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            var cl = clients.Find(p => p.Name + " "+ p.Last_name == comboBoxProvider.SelectedItem.ToString());
+            if (!bindingList.ToList().Exists(x => x.Name == cl.Name))
+                bindingList.Add(new Client(cl));
+        }
+    }
+}
